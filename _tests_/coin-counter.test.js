394bb947df963c97d9799bf762332a58f8bff01e
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-31 15:04:53 -0700

    write test for 2nd spec and pass
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- _tests_/coin-counter.test.js
+++ _tests_/coin-counter.test.js
@@ -4,11 +4,14 @@ import {coinCounter} from './../src/coin-counter.js';
 describe('coinCounter', () => {
   
   test('will return a message if input is not a number', () => {
-    expect(coinCounter("four ninety-nine")).toEqual("Please enter a valid numerical value");
+    expect(coinCounter("four ninety-nine")).toEqual("Please enter a valid numerical value.");
   });
   
-  test('will return a message if input is not valid currency', () => {
-    expect(coinCounter(-1)).toEqual("This is not valid US legal tender");
+  test('will return a message if input is negative', () => {
+    expect(coinCounter(-1)).toEqual("Please enter a positive dollar amount.");
   });
   
+  test('will return a message if input is a fraction of change', () => {
+    expect(coinCounter(1.678)).toEqual("Please only use proper coin amounts.");
+  });
 });
--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,11 +1,14 @@
 export function coinCounter(usd) {
   if (isNaN(usd)) {
-    return "Please enter a valid numerical value";
+    return "Please enter a valid numerical value.";
+  } else if (usd < 0) {
+    return "Please enter a positive dollar amount.";
+
+}else if (usd % .01 !== 0)
+
   } else {
-    
-  
-  
-  
     return "I don't know how to do this yet";
   }
-} 
+}
+
+//var numDec = temp.split(".")[1].length;
