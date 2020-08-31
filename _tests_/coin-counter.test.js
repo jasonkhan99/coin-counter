6435a95d83e0c07088ea0b2dad43d69738d07f44
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-31 14:15:01 -0700

    write test for 2nd spec and fail
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- _tests_/coin-counter.test.js
+++ _tests_/coin-counter.test.js
@@ -6,4 +6,9 @@ describe('coinCounter', () => {
   test('will return a message if input is not a number', () => {
     expect(coinCounter("four ninety-nine")).toEqual("Please enter a valid numerical value");
   });
+  
+  test('will return a message if input is not valid currency', () => {
+    expect(coinCounter(-1)).toEqual("This is not valid US legal tender");
+  });
+  
 });
